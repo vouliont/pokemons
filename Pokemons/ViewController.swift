8360d49fcdf655ff5f2db961464afa66bf1512d4
vouliont <vladporaiko@gmail.com>
2020-09-04 16:18:44 +0300

    Initial Commit

---
+++ Pokemons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemons
+//
+//  Created by Владислав on 04.09.2020.
+//  Copyright © 2020 Владислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
